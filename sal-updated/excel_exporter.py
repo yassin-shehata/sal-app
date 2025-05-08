1562423b7e9f114ce7def3e95e02dcde3ce84500
yassin-shehata <yassin.shehata@ucalgary.ca>
2025-05-08 13:20:11 -0600

    Create excel_exporter.py

---
+++ sal-updated/excel_exporter.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from openpyxl import load_workbook
+import os
+
+def append_or_create_excel(filename, sheet_name, data, headers):
+    df_new = pd.DataFrame(data, columns=headers)
+
+    if not os.path.isfile(filename):
+        with pd.ExcelWriter(filename, engine='openpyxl') as writer:
+            df_new.to_excel(writer, index=False, sheet_name=sheet_name)
+    else:
+        book = load_workbook(filename)
+        with pd.ExcelWriter(filename, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+            writer.book = book
+            if sheet_name in book.sheetnames:
+                existing_df = pd.read_excel(filename, sheet_name=sheet_name)
+                final_df = pd.concat([existing_df, df_new], ignore_index=True)
+                final_df.to_excel(writer, index=False, sheet_name=sheet_name)
+            else:
+                df_new.to_excel(writer, index=False, sheet_name=sheet_name)
